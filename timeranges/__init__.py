f530658625f75aca8721677415232383ec5d4f93
Micael Jarniac <micael@jarniac.com>
2021-12-04 12:07:34 -0300

    fix: `__all__`

--- timeranges/__init__.py
+++ timeranges/__init__.py
@@ -4,3 +4,6 @@ __version__ = "0.3.0"
 
 from ._timeranges import TimeRange, TimeRanges, WeekRange
 from ._types import Weekday
+
+# TODO Maybe generate it programatically?
+__all__ = ["TimeRange", "TimeRanges", "WeekRange", "Weekday"]

e9c19b91fc1f433554cb08a4195595f7938c793c
Micael Jarniac <micael@jarniac.com>
2022-09-05 14:22:08 -0300

    chore: fix comment typo

--- src/timeranges/__init__.py
+++ src/timeranges/__init__.py
@@ -4,5 +4,5 @@ __version__ = "0.7.1"
 
 from ._timeranges import TimeRange, TimeRanges, WeekRange
 
-# TODO Maybe generate it programatically?
+# TODO Maybe generate it programmatically?
 __all__ = ["TimeRange", "TimeRanges", "WeekRange"]
